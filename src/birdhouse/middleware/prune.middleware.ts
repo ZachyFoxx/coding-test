64b2155b1e1380cb3fe54c4fd79eeb48bda8e113
Zachery Elliot <notzachery@gmail.com>
2022-09-23 02:59:18 -0500

    doc: Add more comments

--- src/birdhouse/middleware/prune.middleware.ts
+++ src/birdhouse/middleware/prune.middleware.ts
@@ -10,9 +10,11 @@ export class PruneMiddleware implements NestMiddleware {
   async use(req: any, res: any, next: (error?: any) => void) {
     await (
       await this.birdhouseService.findAll({
+        // The magic number here is one year time in unix epoch.
         where: { last_update: LessThan(new Date().getTime() - 31556926000) },
       })
     ).forEach((house) => {
+      // Prune any houses that haven't received updates in over a year
       this.birdhouseService.delete(house.ubid);
     });
     next();
